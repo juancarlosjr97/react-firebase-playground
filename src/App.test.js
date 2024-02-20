7c76d186f986d945f6bd16a146d79cf1845a082d
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-02-20 22:41:16 +0000

    Updating test to match changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders counter value", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/counter value/i);
   expect(linkElement).toBeInTheDocument();
 });
