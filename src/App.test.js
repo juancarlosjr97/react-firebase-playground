840500bc4bb68e1b4e2ae3403fe01f6c6095b154
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-03-10 13:42:15 +0000

    Adding test for counter action

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from "@testing-library/react";
 import App from "./App";
+import useCounter from "./hooks/useCounter";
+import { render, screen } from "@testing-library/react";
+
+jest.mock("./hooks/useCounter");
 
 test("renders learn react link", () => {
+  useCounter.mockReturnValue({
+    counterValue: 10,
+    loading: false,
+    updateCounterValue: jest.fn(),
+  });
+
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("increments counter value on button click", () => {
+  useCounter.mockReturnValue({
+    counterValue: 10,
+    loading: false,
+    updateCounterValue: jest.fn(),
+  });
+
+  render(<App />);
+
+  const counterElement = screen.getByText(/Counter Value: 10/i);
+  expect(counterElement).toBeInTheDocument();
 });
